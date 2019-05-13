a1b3ceb02bf03250cfc4a43a748ae453b3bedce0
Kshitij Gaipal <ksgaipal@microsoft.com>
2019-05-13 15:25:39 -0700

    line

--- scripts/vs-make.bat
+++ scripts/vs-make.bat
@@ -9,5 +9,5 @@ rem @CALL "C:/Program Files (x86)/Microsoft Visual Studio 14.0/Common7/Tools/VsD
 rem Use for release "/p:Configuration=Release"
 
 :: VS 2017
-@CALL "C:/Program Files (x86)/./Microsoft Visual Studio/2017/Enterprise/Common7/Tools/VsDevCmd.bat"
+@CALL "C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/Common7/Tools/VsDevCmd.bat"
 CALL "msbuild" "/v:minimal" %*
